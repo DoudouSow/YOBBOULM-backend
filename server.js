9854782bc4e8367933c48378dbb3dfd82f481fdd
DoudouSow <dsow2912@gmail.com>
2026-06-30 15:13:02 +0000

    Refactor server.js for improved structure

--- server.js
+++ server.js
@@ -3,11 +3,6 @@ const express = require('express');
 const cors = require('cors');
 const http = require('http');
 const { Server } = require('socket.io');
-const livraisonRoutes = require('./routes/livraisons');
-const gpsRoutes = require('./routes/gps');
-const paiementRoutes = require('./routes/paiements');
-const notificationRoutes = require('./routes/notifications');
-const { initGpsService } = require('./services/gpsService');
 
 const authRoutes = require('./routes/auth');
 const livraisonRoutes = require('./routes/livraisons');
