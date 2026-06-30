49e32854e1b708602b6f2cef57a4ff36739ebaea
DoudouSow <dsow2912@gmail.com>
2026-06-30 15:07:58 +0000

    Remove duplicate route imports in server.js

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const paiementRoutes = require('./routes/paiements');
 const notificationRoutes = require('./routes/notifications');
 const { initGpsService } = require('./services/gpsService');
 
-const authRoutes = require('./routes/auth');
 const livraisonRoutes = require('./routes/livraisons');
 const gpsRoutes = require('./routes/gps');
 const paiementRoutes = require('./routes/paiements');
